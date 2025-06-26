80cb6187ce5327f24be132617b78aa148c3bf3e0
Taradwimahendra15 <taradwimahendra15@gmail.com>
2025-06-26 16:03:28 +0700

    untuk preorder parent diawal baru anak kiri lalu anak kanan

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -91,4 +91,19 @@ class BinaryTree
         cout << ptr->info << " "; 
         inorder(ptr->rightchild);
     }
+
+     void preorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is Empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+            return;
+
+        cout << ptr->info << " ";
+        preorder(ptr->leftchild);
+        preorder(ptr->rightchild);
+    }
 };
